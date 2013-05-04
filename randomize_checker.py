5c3f5ab136a0f577c40a7b3ced22d9c49e76f652
kevintee <kevintee@berkeley.edu>
2013-05-04 03:36:09 -0700

    Randomized stuff, hill climbing is still being annoying, will fix tomorrow

--- randomize_checker.py
+++ randomize_checker.py
@@ -5,6 +5,7 @@ Peter Gao has volunteered to translate this code into Go.
 
 
 '''
+import random
 from random import random
 
 # Read input
@@ -46,3 +47,28 @@ while True:
             i += 1
     check_if_mlst()
 
+    # Method 3: Generate a LST, greater amortized time
+    i = 0
+    mlst = []
+    nodes = set(d.keys())
+    visited_nodes = set([])
+    visited_connected_nodes = set([])
+
+    while i < len(d.keys()) - 1:
+        import random as rand
+        random_node = rand.sample(nodes, 1)[0]
+
+        edges = d[random_node]
+        connecting_node = edges[int(random()*len(edges))]
+
+        if not (random_node in visited_connected_nodes and connecting_node in visited_nodes):
+            nodes.remove(random_node)
+            visited_nodes.add(random_node)
+            visited_connected_nodes.add(connecting_node)
+            mlst.append((random_node, connecting_node))
+            i += 1
+    check_if_mlst()
+
+    # Method 4: Hillclimbing
+
+    break
